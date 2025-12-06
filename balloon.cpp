ae205e9231eb5fab66e4a9603f34fcc495fc0d62
roman4ik <romaingotovkin@yandex.ru>
2025-12-06 09:18:23 +0300

    удаление ненужных файлов

--- balloon.cpp
+++
@@ -1,8 +0,0 @@
-#include "balloon.h"
-
-Balloon::Balloon(const QPoint &position, int size, const QColor &color,
-                 qint64 spawnTime, int lifetime)
-    : m_position(position), m_size(size), m_color(color),
-      m_spawnTime(spawnTime), m_lifetime(lifetime)
-{
-}
